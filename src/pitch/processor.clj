1ab6811b2567de2e2f29d42c6e85f7248f6ae3bc
Agata Sumowska <agata.sumowska@gmail.com>
2023-07-10 13:19:51 +0200

    refactor complete

--- src/pitch/processor.clj
+++ src/pitch/processor.clj
@@ -3,30 +3,16 @@
             [pitch.digits :as digits]
             [pitch.validation :as validation]))
 
-
-
-
-(defn divide-lines-by-3-chars [lines]
-  (map #(partition 3 %) lines))
-
-(defn collect-chars-for-digit [three-levels-nested-lines]
-  (cons (fn [first-line second-line third-line] [digits/seven-segment-digits first-line second-line third-line]) three-levels-nested-lines))
-
-(defn get-seven-segment-digits [seven-segment-line]
-  (->> seven-segment-line
-       divide-lines-by-3-chars
-       collect-chars-for-digit))
-
-(defn seven-segment->digit
-  [seven-segment-digit]
-  (digits/seven-segment-digits seven-segment-digit "?"))
-
 (defn seven-segment-line->digits
   [line]
   (->>
    line
-   get-seven-segment-digits 
-   (mapv seven-segment->digit)))
+   (map #(->>
+          %
+          seq
+          ((fn [x] (partition 3 x)))))
+   (cons #(digits/chars-digits [%1 %2 %3] \?))
+   (apply mapv)))
 
 (defn parse-validation [digits-line]
   (cond
@@ -45,22 +31,6 @@
    (s/join  (System/getProperty "line.separator"))))
 
 
-(comment 
-  
-  (->> (slurp "test/pitch/resources/sample_1.txt")
-       (s/split-lines)
-       (partition 3 4)
-       (map (fn [line-of-digits]
-              (->>
-               line-of-digits
-               (map #(->>  %
-                           seq
-                           ( (fn [x] (partition 3 x)))))
-               (cons #(digits/chars-digits [%1 %2 %3] \?))
-               (apply map)
-               )))
-       
-       
-       ))
+
 
 
--- test/pitch/processor_test.clj
+++ test/pitch/processor_test.clj
@@ -2,29 +2,6 @@
   (:require [clojure.test :refer :all]
             [pitch.processor :refer :all]))
 
-(deftest seven-segment->digit-test
-  (testing "translates a 0 seven-segment digit to a digit"
-    (let [seven-segment-zero
-" _ 
-| |
-|_|"
-          result (seven-segment->digit seven-segment-zero)]
-    (is (= 0 result))))
-  (testing "translates a 1 seven-segment digit to a digit"
-    (let [seven-segment-one
-"   
-  |
-  |"
-          result (seven-segment->digit seven-segment-one)]
-      (is (= 1 result))))
-  (testing "translates a  seven-segment digit with a missing space to a question mark"
-    (let [seven-segment-one
-          "   
-    |
-  |"
-          result (seven-segment->digit seven-segment-one)]
-      (is (= "?" result)))))
-
 (deftest seven-segment-line->digits-test
   (testing "parses one line of input"
     (let [seven-segment-line
@@ -32,14 +9,14 @@
            "| |  | _| _||_||_ |_   ||_||_|"
            "|_|  ||_  _|  | _||_|  ||_| _|"]
           result (seven-segment-line->digits seven-segment-line)]
-      (is (= [0 1 2 3 4 5 6 7 8 9] result))))
+      (is (= `(0 1 2 3 4 5 6 7 8 9) result))))
   (testing "parses one line of input"
     (let [seven-segment-line
           [" _     _  _     _  _  _  _  _ "
            "| |  | _| _||_||_ |_   ||_||_|"
            "|_|  ||_  _|  | _||_|  ||_| _ "]
           result (seven-segment-line->digits seven-segment-line)]
-      (is (= [0 1 2 3 4 5 6 7 8 "?"] result)))))
+      (is (= `(0 1 2 3 4 5 6 7 8 \?) result)))))
 
 (deftest parse-validation-test 
   (testing "add ERR to invalid checksum line"
