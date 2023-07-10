fdf1200c1a96ae22924e876bfae00f38cc70156d
Agata Sumowska <agata.sumowska@gmail.com>
2023-07-10 12:51:50 +0200

    validation variation

--- src/pitch/validation.clj
+++ src/pitch/validation.clj
@@ -8,5 +8,11 @@
                        0  input)]
     (not= 0 (mod sum 11))))
 
-(defn not-only-digits? [input]
-  (some? (some (fn [x] (not (number? x))) input)))
+(defn elem-not-in-the-list [expected-elems elem]
+  (empty? (filter expected-elems elem)))
+
+(defn unexpected-char? [input]
+ (reduce (fn[acc char]
+           (if (elem-not-in-the-list #{0 1 2 3 4 5 6 7 8 9} #{char})
+             (reduced true)
+             false)) false input))
